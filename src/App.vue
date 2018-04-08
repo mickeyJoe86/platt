ea42f2017d33225de481b9309ec70e18d5901772
Mike Ward <ward.michael51@gmail.com>
2018-04-08 15:12:02 -0700

    feat(PlattNavList): init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
     <PlattHeader />
+    <PlattNavList />
     <PlattFooter />
   </div>
 </template>
 
 <script>
-import PlattHeader from './components/PlattHeader.vue'
-import PlattFooter from './components/PlattFooter.vue'
-
+import PlattHeader from './components/PlattHeader'
+import PlattFooter from './components/PlattFooter'
+import PlattNavList from './components/PlattNavList';
 export default {
   name: 'app',
   components: {
     PlattHeader,
-    PlattFooter
+    PlattFooter,
+    PlattNavList
   }
 }
 </script>
---
+++ src/components/PlattNavList.vue
@@ -0,0 +1,97 @@
+<template>
+  <ul class="nav-list">
+    <li v-for="(item, i) in navList"
+      :key="i"
+      v-bind:class="{ 'lead-item': i === 0 }"
+      class="nav-list-item">
+      <a href="#" class="nav-list-item-link">
+        <img 
+          class="nav-list-item-link-img"
+          :src="item.iconPath" />
+          {{ item.text }}
+        <img 
+          class="nav-list-item-link-img"
+          src="/assets/icons/black/ic_arrow_forward.png" />
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+const iconRootPath = "/assets/icons/black/";
+export default {
+  name: "PlattNavList",
+  data() {
+    return {
+      navList: [
+        {
+          text: "Shop all categories",
+          iconPath: `${iconRootPath}ic_categories.png`
+        },
+        {
+          text: "My catalog",
+          iconPath: `${iconRootPath}ic_playlist_add_check.png`
+        },
+        {
+          text: "My List",
+          iconPath: `${iconRootPath}ic_list.png`
+        },
+        {
+          text: "Live Help",
+          iconPath: "/assets/icons/green/ic_chat-active.png"
+        },
+        {
+          text: "PIM",
+          iconPath: `${iconRootPath}ic_check_circle.png`
+        },
+        {
+          text: "Account",
+          iconPath: `${iconRootPath}ic_place.png`
+        },
+        {
+          text: "Branch Locations",
+          iconPath: `${iconRootPath}ic_place.png`
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../scss/index";
+.nav-list {
+  padding: 16px 10px;
+
+  &-item {
+    border-top: solid 1px #aeaeae;
+    padding: 0 32px 0 16px;
+
+    &-link {
+      @include link-primary-dark;
+      display: block;
+      padding: 12px 0 12px 0;
+      font-size: 1.1em;
+      height: 100%;
+      width: 100%; 
+      font-weight: lighter;
+
+      &-img:first-of-type {
+        margin-right: 8px;
+      }
+      &-img:last-of-type {
+        float: right;
+      }
+    }
+
+    &.lead-item {
+      border: none;
+      padding-bottom: 18px;
+
+      .nav-list-item-link {
+        font-weight: 800;
+      }
+    }
+  }
+}
+</style>
